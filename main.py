cd6fb7e4732ec037831ad772a6a0af2d62b75be5
Syd Shearer <65626074+sshearer2000@users.noreply.github.com>
2020-05-29 12:48:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,115 +34,11 @@ To be able to import csv files, make sure to upload files first via the tabs on 
 stud = pd.read_csv('Difficulty Protocol Data - Student Protocol.csv',encoding="utf-8",index_col=0)
 profta = pd.read_csv('Difficulty Protocol Data - Faculty_TA Protocol.csv',encoding="utf-8",index_col=0)
 
-"""# Tokenize function"""
-
-#sentence tokenizer for bag of words and n-grams, returns tokenized sentences 
-#for each column
-def tokenize(csv,col):
-    whole_text = ''
-    for sentence in csv[col]:
-        if not str(sentence).endswith(string.punctuation):
-            sentence+='. '
-            whole_text += str(sentence)
-    tokenized = nltk.sent_tokenize(whole_text)
-    return tokenized
-
 """# Stop Word Categories
 
 1.   Getting rid of any descriptor words (i.e. briefly, basically, current, towards)
 2.   Getting rid of typical classroom activities (i.e. talked, presented, covering)
 3. Getting rid of words that don’t give any insight (i.e. barely, spent, able, end, began)
-
-# Bag of words function
-"""
-
-#creates a bag of words for each function
-def bag_of_words(tokenized):
-    #extends stop words list with context specific frequent words
-    stop_words = list(stopwords.words('english'))
-    ext = ['2','briefly','primarily','talked','discussed','continuing',
-           'students','discuss','spent','able','barely','end','covering',
-           'basically','current','towards','began','none','na','n','nope','pre',
-           'vs','mainly','sick','lots','weeks']
-    stop_words.extend(ext)
-    #removes non-alphabetical characters and white spaces
-    for i in range(len(tokenized)):
-        tokenized[i]=tokenized[i].lower()
-        tokenized[i] = re.sub(r'\W',' ',tokenized[i])
-        tokenized[i] = re.sub(r'\s+', ' ', tokenized[i])
-    #removes blank answers
-    for i in reversed(range(len(tokenized))):
-        if tokenized[i]==' ':
-            del tokenized[i]
-    #creates list of lists, inside lists contains sentences tokenized by word
-    list_of_lists = []
-    for sentence in tokenized:
-        tokens = nltk.word_tokenize(sentence)
-        sentence_list = []
-        #removes stopwords
-        for token in tokens:
-          if token in stop_words:
-            continue
-          else:
-            sentence_list.append(token)
-        list_of_lists.append(sentence_list)
-    return list_of_lists
-
-"""# Create n-grams function"""
-
-#creates bag of n-grams
-def extract_ngrams(col,num):
-    #creates a list of lists of the sentences tokenized by word
-    list_of_lists = []
-    #extends stop words with frequent unneccesary words
-    stop_words = list(stopwords.words('english'))
-    #descriptor words (i.e. briefly, basically, current, towards)
-    des = ['briefly','mostly','basically','different','forward','primarily',
-           'barely','first','many','new','much','already','heavily','hardest',
-           'actually','particularly','really','best','lot','extremely','super',
-           'unclear','previous','good','clearly','exactly','better','previosuly',
-           'less','early','next','mainly','hard','frustrating','well','curious',
-           'enough','surprised','kinda','scary','lots','late','fair','seemingly',
-           'difficult','actual','old','biggest','specific','successfully',
-           'comfortable','sufficient','long','clear','minimally','cool']
-    #typical classroom activities (i.e. talked, presented, covering)
-    acts = ['worked','covered','period','class','zoom','applying','finishing',
-            'continue','discussing','discuss','discussed','learned','practice',
-            'covering','writing','week','students','contine','focused','devoted',
-            'practiced','peers','struggled','semester','confused','know',
-            'understand','asked','babynames','campaign','baumer','asking','told',
-            'weeks','students','material','professor','work','questions','thought',
-            'cover','spoke','grasping','picked','spent','people','peoples','bmi',
-            'encountered','help','focus','peer','group','looking','problems',
-            'course','campus','grasp','person','struggle','talked','related',
-            'based','mentioned','clark','learn']
-    #words that don’t give any insight (i.e. spent, able, end, began,vs, etc)
-    etc = ['etc','also','began','time','around','5','2','able','holding',
-           'animal','shelter','going','added','changed','end','towards','pros',
-           'cons','like','1','3','amount','number','vs','bit','anything','getting',
-           'although','could','get','anyone','still','4','past','us','everything',
-           'figured','e','put','because','n','part','trouble','im','didnt','comes',
-           'mind','cant','let','none','hitting','issues','way','choose','wayyyy',
-           'came','love','go','since','though','something','would','within','along',
-           'hear','look','see','remember','nothing','stands','other','others',
-           'need','givern','certain','kinds','matters','among','across','come',
-           'feel','putting','opinion','manner','needed','moving','arise',
-           'constitutes','made','kind','brought','one','never','seemed','pain',
-           'heard','reality','two','ever','difficulties','works','however','knows']
-    stop_words.extend(des)
-    stop_words.extend(acts)
-    stop_words.extend(etc)
-    for sentence in col:
-      new_sent = ''
-      for word in sentence.split():
-        #removes stop words
-        if word not in stop_words:
-          new_sent += word
-          new_sent += ' '
-      n_grams=ngrams(nltk.word_tokenize(new_sent),num)
-      ret = [' '.join(grams) for grams in n_grams]
-      list_of_lists.append(ret)
-    return list_of_lists
 
 """# Create student bags of words"""
 

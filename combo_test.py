02311cfdb18cf63b5b7e9f2f910e3114335b5321
Syd Shearer <65626074+sshearer2000@users.noreply.github.com>
2020-05-30 17:38:40 -0500

    Update combo_test.py

--- combo_test.py
+++ combo_test.py
@@ -102,3 +102,82 @@ for group in ["stud", "profta","]:
 
     # save the string of useful words (without the whitespace at the end)
     lemm_answers[q][group].append(processed_str)
+              
+# number M of top words by TF IDF to display
+M = 8
+
+for question in qs:
+  print("Question: %s --------------------" % question)
+
+  # load student answers
+  stu_ans = lemm_answers[question]["stud"]
+  # concatenate into string, remove quotation marks, and remove square brackets
+  stu_ans = str(stud_ans)[1:-1].replace("'", "")  
+
+  # load professor answers
+  profta_ans = lemm_answers[question]["profta"]
+  # concatenate into string, remove quotation marks, and remove square brackets
+  profta_ans = str(profta_ans)[1:-1].replace("'", "")  
+
+  corpus = [stud_ans, profta_ans]
+
+  # compute TF-IDF of words in the 3 documents
+  vectorizer = TfidfVectorizer()
+  X = vectorizer.fit_transform(corpus)
+  X = X.todense()
+  words = vectorizer.get_feature_names()
+
+  print("Unique words in answers to '%s': %d." % (question, X.shape[1]))
+
+  # print top M words by TF-IDF score
+  X = np.asarray(X)
+
+  group_names = ["stud", "profta"]
+  top_words = {"stud": [], "profta": []}
+
+  # find the M terms with the highest TF-IDF scores
+  for i in range(X.shape[0]):
+    doc_vals = X[i]
+    srt = np.argsort(doc_vals)[::-1][:M]
+    
+    # print the M words with highest TF-IDF scores
+    for j in range(M):
+      top_words[group_names[i]].append(words[srt[j]])
+
+  # container for counting frequencies of high TF-IDF words
+  top_words_freqs = {"stu": [], "ta": [], "prof": []}
+
+  for group in group_names:
+    for w in range(len(top_words[group])):
+      word = top_words[group][w]
+
+      # counter for how many rows the word appears in
+      encountered = 0  
+
+      for i in range(len(lemm_answers[question][group])):
+        enc = False
+        answer = nlp(lemm_answers[question][group][i])
+
+        for token in answer:
+          # if at least one of the word tokens is equal to the word, encountered
+          if word == token.text:
+            enc = True
+        
+        if enc: encountered += 1
+
+      # append the word and the frequency (rows encountered/total rows)
+      top_words_freqs[group].append("%s (%d/%d)" % (word, encountered, len(lemm_answers[question][group])))
+
+  # write result for this question to a Pandas dataframe
+  out_df = pd.DataFrame()
+  for key in top_words_freqs:
+    out_df[key] = top_words_freqs[key]
+  out_df.to_csv(data_path + "top_terms/" + question + ".csv", index=False)
+
+  # print the high TF-IDF words and their associated row frequencies
+  for group in group_names:
+    print(group)
+    for i in range(len(top_words_freqs[group])):
+      print("%d. %s." % (i+1, top_words_freqs[group][i]))
+
+  print("---------------------------\n")    
